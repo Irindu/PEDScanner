8cfa44a455bfce4dff640e92b2b84315c9926bed
Keerthana <keerthyratnam@gmail.com>
2018-09-29 08:31:22 -0700

    Changed the getAssemblyDependencies function and new function IsManagedAssembly Added

--- PEDScannerLib/PEDScannerLib/PortableExecutable.cs
+++ PEDScannerLib/PEDScannerLib/PortableExecutable.cs
@@ -114,37 +114,38 @@ namespace PEDScannerLib.Core
         /// <returns></returns>
         public List<ImportFunctionObject> GetAssemblyDependencies(string FilePath)
         {
-            try
+            if (FilePath != null)
             {
-                assembly = Assembly.LoadFrom(FilePath);
-                Dictionary<Type, List<MethodInfo>> exports = assembly.GetTypes()
-                  .SelectMany(type1 => type1.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static)
-                                          .Where(method => method.GetCustomAttributes(typeof(DllImportAttribute), false).Length > 0))
-                  .GroupBy(method => method.DeclaringType)
-                  .ToDictionary(item => item.Key, item => item.ToList())
-                  ;
-
-                foreach (var item in exports)
+                if (IsManagedAssembly(FilePath))
                 {
-                    foreach (var method in item.Value)
+                    assembly = Assembly.LoadFrom(FilePath);
+                    Dictionary<Type, List<MethodInfo>> exports = assembly.GetTypes()
+                      .SelectMany(type1 => type1.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static)
+                                              .Where(method => method.GetCustomAttributes(typeof(DllImportAttribute), false).Length > 0))
+                      .GroupBy(method => method.DeclaringType)
+                      .ToDictionary(item => item.Key, item => item.ToList())
+                      ;
+
+                    foreach (var item in exports)
                     {
+                        foreach (var method in item.Value)
+                        {
 
-                        DllImportAttribute attr = method.GetCustomAttributes(typeof(DllImportAttribute), false)[0] as DllImportAttribute;
+                            DllImportAttribute attr = method.GetCustomAttributes(typeof(DllImportAttribute), false)[0] as DllImportAttribute;
 
-                        ImportFunctions.Add(new ImportFunctionObject(method.Name, 0, attr.Value));
-                        ImportNames.Add(attr.Value);
+                            ImportFunctions.Add(new ImportFunctionObject(method.Name, 0, attr.Value));
+                            ImportNames.Add(attr.Value);
+                        }
+                    }
+
+                    // Find the set of assemblies our assemblies reference
+                    foreach (AssemblyName an in assembly.GetReferencedAssemblies())
+                    {
+                        ImportNames.Add((an.Name + ".dll"));
                     }
                 }
+            }
 
-                // Find the set of assemblies our assemblies reference
-                foreach (AssemblyName an in assembly.GetReferencedAssemblies())
-                {
-                    ImportNames.Add((an.Name + ".dll"));
-                }
-            }
-            catch (Exception)
-            {
-            }
             return ImportFunctions;
         }
 
@@ -500,6 +501,75 @@ namespace PEDScannerLib.Core
                 default:
                     return "Machine type Unknown";
                     break;
+            }
+        }
+
+        /// <summary>
+        /// check whether the dll or exe is managed or not
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static bool IsManagedAssembly(string fileName)
+        {
+            using (Stream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binaryReader = new BinaryReader(fileStream))
+            {
+                if (fileStream.Length < 64)
+                {
+                    return false;
+                }
+
+                //PE Header starts @ 0x3C (60). Its a 4 byte header.
+                fileStream.Position = 0x3C;
+                uint peHeaderPointer = binaryReader.ReadUInt32();
+                if (peHeaderPointer == 0)
+                {
+                    peHeaderPointer = 0x80;
+                }
+
+                // Ensure there is at least enough room for the following structures:
+                //     24 byte PE Signature & Header
+                //     28 byte Standard Fields         (24 bytes for PE32+)
+                //     68 byte NT Fields               (88 bytes for PE32+)
+                // >= 128 byte Data Dictionary Table
+                if (peHeaderPointer > fileStream.Length - 256)
+                {
+                    return false;
+                }
+
+                // Check the PE signature.  Should equal 'PE\0\0'.
+                fileStream.Position = peHeaderPointer;
+                uint peHeaderSignature = binaryReader.ReadUInt32();
+                if (peHeaderSignature != 0x00004550)
+                {
+                    return false;
+                }
+
+                // skip over the PEHeader fields
+                fileStream.Position += 20;
+
+                const ushort PE32 = 0x10b;
+                const ushort PE32Plus = 0x20b;
+
+                // Read PE magic number from Standard Fields to determine format.
+                var peFormat = binaryReader.ReadUInt16();
+                if (peFormat != PE32 && peFormat != PE32Plus)
+                {
+                    return false;
+                }
+
+                // Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
+                // When this is non-zero then the file contains CLI data otherwise not.
+                ushort dataDictionaryStart = (ushort)(peHeaderPointer + (peFormat == PE32 ? 232 : 248));
+                fileStream.Position = dataDictionaryStart;
+
+                uint cliHeaderRva = binaryReader.ReadUInt32();
+                if (cliHeaderRva == 0)
+                {
+                    return false;
+                }
+
+                return true;
             }
         }
     }
